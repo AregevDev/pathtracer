311f69fc398a289428cdfacd23e935bb4967f3ab
Alon Regev <aregevdev@gmail.com>
2019-12-05 20:31:02 +0200

    Add more operator implementations for Vector3

--- src/vector.rs
+++ src/vector.rs
@@ -69,11 +69,31 @@ impl Add<Vector3> for Vector3 {
     }
 }
 
+impl Add<f32> for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, rhs: f32) -> Self::Output {
+        Vector3 {
+            x: self.x + rhs,
+            y: self.y + rhs,
+            z: self.z + rhs,
+        }
+    }
+}
+
 impl AddAssign<Vector3> for Vector3 {
     fn add_assign(&mut self, rhs: Vector3) {
         self.x += rhs.x;
         self.y += rhs.y;
         self.z += rhs.z;
+    }
+}
+
+impl AddAssign<f32> for Vector3 {
+    fn add_assign(&mut self, rhs: f32) {
+        self.x += rhs;
+        self.y += rhs;
+        self.z += rhs;
     }
 }
 
@@ -89,11 +109,43 @@ impl Sub<Vector3> for Vector3 {
     }
 }
 
+impl Sub<f32> for Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, rhs: f32) -> Self::Output {
+        Vector3 {
+            x: self.x - rhs,
+            y: self.y - rhs,
+            z: self.z - rhs,
+        }
+    }
+}
+
 impl SubAssign<Vector3> for Vector3 {
     fn sub_assign(&mut self, rhs: Vector3) {
         self.x -= rhs.x;
         self.y -= rhs.y;
         self.z -= rhs.z;
+    }
+}
+
+impl SubAssign<f32> for Vector3 {
+    fn sub_assign(&mut self, rhs: f32) {
+        self.x -= rhs;
+        self.y -= rhs;
+        self.z -= rhs;
+    }
+}
+
+impl Mul<Vector3> for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: Vector3) -> Self::Output {
+        Vector3 {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        }
     }
 }
 
@@ -109,11 +161,31 @@ impl Mul<f32> for Vector3 {
     }
 }
 
+impl MulAssign<Vector3> for Vector3 {
+    fn mul_assign(&mut self, rhs: Vector3) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+        self.z *= rhs.z;
+    }
+}
+
 impl MulAssign<f32> for Vector3 {
     fn mul_assign(&mut self, rhs: f32) {
         self.x *= rhs;
         self.y *= rhs;
         self.z *= rhs;
+    }
+}
+
+impl Div<Vector3> for Vector3 {
+    type Output = Vector3;
+
+    fn div(self, rhs: Vector3) -> Self::Output {
+        Vector3 {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y,
+            z: self.z / rhs.z,
+        }
     }
 }
 
@@ -126,6 +198,14 @@ impl Div<f32> for Vector3 {
             y: self.y / rhs,
             z: self.z / rhs,
         }
+    }
+}
+
+impl DivAssign<Vector3> for Vector3 {
+    fn div_assign(&mut self, rhs: Vector3) {
+        self.x /= rhs.x;
+        self.y /= rhs.y;
+        self.z /= rhs.z;
     }
 }
 
