08510865c6709a1fe968212c7c5715d91a3d8c62
Alon Regev <aregevdev@gmail.com>
2019-12-05 21:20:09 +0200

    Multiple spheres

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
-mod ray;
-mod vector;
+use std::fmt::Write;
+use std::fs;
 
 use ray::Ray;
 use vector::Vector3;
 
-use std::fmt::Write;
-use std::fs;
-use std::iter;
+mod ray;
+mod vector;
+
+fn hit_sphere(center: Vector3, radius: f32, r: Ray) -> bool {
+    let oc = r.origin - center;
+    let a = r.direction.dot(&r.direction);
+    let b = 2.0 * oc.dot(&r.direction);
+    let c = oc.dot(&oc) - radius * radius;
+
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant > 0.0
+}
 
 fn compute_color(r: Ray) -> Vector3 {
+    // Draw a pastel red sphere
+    if hit_sphere(Vector3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Vector3::new(1.0, 0.0, 0.0) * 0.5 + 0.5;
+    }
+
+    // Draw pastel blue sphere
+    if hit_sphere(Vector3::new(-0.5, 0.0, -1.0), 0.5, r) {
+        return Vector3::new(0.0, 0.0, 1.0) * 0.5 + 0.5;
+    }
+
+    // Draw pastel green sphere
+    if hit_sphere(Vector3::new(0.5, 0.0, -1.0), 0.5, r) {
+        return Vector3::new(0.0, 1.0, 0.0) * 0.5 + 0.5;
+    }
+
     let unit_dir = r.direction.normalize();
     let t = 0.5 * (unit_dir.y + 1.0);
-    Vector3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vector3::new(0.5, 0.7, 1.0) * t
+    Vector3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vector3::new(0.5, 0.5, 1.0) * t
 }
 
 fn main() {
@@ -23,8 +48,8 @@ fn main() {
     let height = 500;
 
     // Define camera vectors
-    let ll_corner = Vector3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vector3::new(4.0, 0.0, 0.0);
+    let ll_corner = Vector3::new(-1.0, -1.0, -1.0);
+    let horizontal = Vector3::new(2.0, 0.0, 0.0);
     let vertical = Vector3::new(0.0, 2.0, 0.0);
     let origin = Vector3::new(0.0, 0.0, 0.0);
 
